ac39fc700930bd6c502dd3dc8a0d671ef8863153
Thomas Laird <t6r6l5@gmail.com>
2017-10-11 13:29:26 -0600

    mushroom tests

--- test/Mushrooms-test.js
+++ test/Mushrooms-test.js
@@ -1,9 +1,16 @@
 const Mushrooms = require('../lib/Mushrooms.js');
 const chai = require('chai');
 const assert = chai.assert;
+let mushroom;
 
 describe('Mushrooms', function() {
-  it('', function() {
-    assert.equal(true,true);
+  beforeEach( () => {
+    mushroom = new Mushrooms(48,96,4);
   });
-})
+  
+  it('Should be instantiated at the values passed in to it', function() {
+    assert.equal(mushroom.x, 48);
+    assert.equal(mushroom.y, 96);
+    assert.equal(mushroom.level, 4);
+  });
+});
